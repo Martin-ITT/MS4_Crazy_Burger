4bf50dc201508e9704d22b792b2a7419b6dfcf3c
Martin-ITT <martinusko@gmail.com>
2021-09-10 12:33:20 +0000

    shopping bag app added with urls and template

--- products/models.py
+++ products/models.py
@@ -28,7 +28,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    allergens = models.ManyToManyField(Allergen, null=True, blank=True)
+    allergens = models.ManyToManyField(Allergen, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
