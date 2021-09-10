bb7439d02e81afe2fe02cc963ea840f6cb05414b
Martin-ITT <martinusko@gmail.com>
2021-09-10 09:46:56 +0000

    js for sort by button added

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import (
     render, redirect, reverse, get_object_or_404)
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -23,7 +24,7 @@ def all_products(request):
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
-                sortkey = 'lower'
+                sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
