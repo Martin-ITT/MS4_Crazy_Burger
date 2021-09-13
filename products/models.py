3d54e19eec3aa413e3eceeee8e41cb48f81f6145
Martin-ITT <martinusko@gmail.com>
2021-09-13 12:26:59 +0000

    meal option in product detail added

--- products/models.py
+++ products/models.py
@@ -30,6 +30,7 @@ class Product(models.Model):
     description = models.TextField()
     allergens = models.ManyToManyField(Allergen, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    price_meal = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
