1564bc625ba436cd7b84b19ea7733c6865d019b5
Martin-ITT <martinusko@gmail.com>
2021-09-21 18:46:19 +0000

    menu restyled, shortcuts to categories added in navbar

--- bag/views.py
+++ bag/views.py
@@ -15,7 +15,7 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
-    drink = None
+    drink = ""
 
     # variable to pass some product data to context.py
     product_data = None
--- products/views.py
+++ products/views.py
@@ -17,6 +17,7 @@ def all_products(request):
     categories = None
     sort = None
     direction = None
+    display_categories = None
 
     """ sorting by"""
     if request.GET:
@@ -37,7 +38,29 @@ def all_products(request):
             products = products.order_by(sortkey)
 
         """ selecting categories in navbar """
+
         if 'category' in request.GET:
+            category = request.GET['category']
+            category_food = ['burgers', 'pizza', 'loaded_chips', 'salads', 'creoles', 'kebabs', 'wraps', 'baguettes', 'china_town', 'fish', 'sides']
+            category_meals_deals = ['kids_meals', 'daily_offers', 'deals', 'meals']
+            category_deserts = ['ice_cream', 'crepes', 'milkshakes', 'other_deserts']
+            category_other = ['new_products', 'specials', 'drinks', 'extras']
+            display_categories = request.GET['category'].split(',')
+            if category in category_food:
+                display_categories = category_food
+                display_categories = Category.objects.filter(name__in=display_categories)
+            if category in category_meals_deals:
+                display_categories = category_meals_deals
+                display_categories = Category.objects.filter(name__in=display_categories)
+            if category in category_deserts:
+                display_categories = category_deserts
+                display_categories = Category.objects.filter(name__in=display_categories)
+            if category in category_other:
+                display_categories = category_other
+                display_categories = Category.objects.filter(name__in=display_categories)
+
+        if 'category' in request.GET:
+            category = request.GET['category']
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
@@ -61,6 +84,7 @@ def all_products(request):
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
+        'display_categories': display_categories,
     }
 
     return render(request, 'products/products.html', context)
