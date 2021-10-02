f47e0b4f1ff81a4a4b0809cebdb8d9aa01e8a65c
Martin-ITT <martinusko@gmail.com>
2021-10-02 21:05:12 +0000

    user profiles added

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,7 +20,6 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
@@ -31,11 +30,12 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             # style in checkout.css
             self.fields[field].widget.attrs['class'] = (
                 'stripe-style-input w-100 mb-1')
