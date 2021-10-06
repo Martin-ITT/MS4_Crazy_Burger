34795f57bc7ee37dd4a24d985c3a2d5f4e0134cb
Martin-ITT <martinusko@gmail.com>
2021-10-06 20:17:17 +0000

    removed secret key and debug set

--- crazy_burger/settings.py
+++ crazy_burger/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sl=^_bz)gkul^i=&_n8)t$(n#93@($#(%t!r8=%#&6!4q4xwcf'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['crazyburger.herokuapp.com', 'localhost']
 
